94cd2e3d0625217a3e3f8ed95b048062e88f2450
alexHooper <alexmohack@gmail.com>
2018-09-04 23:32:22 +0530

    use bs4 to prettify response

--- requesting_yelp.py
+++ requesting_yelp.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://www.yelp.com/search?find_desc=Restaurants&find_loc=San+Francisco%2C+CA&ns=1"
 response = requests.get(url)
@@ -6,3 +7,6 @@ response = requests.get(url)
 print(response)
 print(f"STATUS CODE: {response.status_code}")
 print(response.text)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup.prettify())
