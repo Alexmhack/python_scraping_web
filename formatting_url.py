53ecab939efced737a70127f0426802f4b1f02b6
alexHooper <alexmohack@gmail.com>
2018-09-07 12:40:41 +0530

    add logic for requesting next page in results

--- formatting_url.py
+++ formatting_url.py
@@ -3,12 +3,15 @@ from bs4 import BeautifulSoup
 
 base_url = "https://www.yelp.com/search?find_desc=Restaurants&find_loc={}"
 city = "los angeles"
+start = 120
 
 url = base_url.format(city)
 
-response = requests.get(url)
+third_page = url + '&start=' + str(start)
 
-print(f"STATUS CODE: {response.status_code}")
+response = requests.get(third_page)
+
+print(f"STATUS CODE: {response.status_code} FOR {response.url}")
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
