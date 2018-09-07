ba14f73c36d15f6e004c13cee9eef7091f4bbd38
alexHooper <alexmohack@gmail.com>
2018-09-07 14:48:57 +0530

    create file for extracting restaurant details

---
+++ extract_details.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+base_url = "https://www.yelp.com/search?find_desc=Restaurants&find_loc={}"
+city = "los angeles"
+start = 30
+
+url = base_url.format(city)
+
+third_page = url + '&start=' + str(start)
+
+response = requests.get(third_page)
+
+print(f"STATUS CODE: {response.status_code} FOR {response.url}")
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+businesses = soup.findAll('div', {'class': 'biz-listing-large'})
+
+count = 0
+for info in businesses:
+	print(info.text)
+	count += 1
+
+print(count)
