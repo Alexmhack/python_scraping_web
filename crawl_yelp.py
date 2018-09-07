0ad3a5eea4d406d385380cbf9942c859f30443f2
alexHooper <alexmohack@gmail.com>
2018-09-07 13:32:19 +0530

    add logic for requesting url and writing unique urls in file

--- crawl_yelp.py
+++ crawl_yelp.py
@@ -8,3 +8,29 @@ url = base_url.format(city)
 
 with open('yelp_20_pages.txt', 'w') as file:
 	count = 0
+	all_links = []
+	start = 0
+
+	for i in range(100):
+		url += '&start=' + str(start)
+		response = requests.get(url)
+		start += 30
+		if start == 540:
+			break
+		print(f"STATUS CODE: {response.status_code} FOR {response.url}")
+		soup = BeautifulSoup(response.text, 'html.parser')
+		links = soup.findAll('a')
+		for link in links:
+			if response.status_code == 200:
+				href = link.get('href', '')
+				if 'https:' in href:
+					if href not in all_links:
+						all_links.append(href)
+						print(href)
+						file.write(href + '\n\n')
+						count += 1
+					else:
+						print('ALREADY EXISTS...')
+			else:
+				print('404 -- SKIPPING')
+	print(f"{count} REQUESTED...")
